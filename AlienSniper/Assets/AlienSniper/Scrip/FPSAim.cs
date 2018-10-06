8f63134a9fa1c0132d6d23f9dd8dcced09f2dcc1
abraham-tl <abraham-tl>
2018-10-05 20:56:01 -0500

    se inicio
    se inicio

---
+++ AlienSniper/Assets/AlienSniper/Scrip/FPSAim.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public sealed class FPSAim : MonoBehaviour {
+    public Text hola;
+    //float mousex;
+    //float mousey;
+    //public bool invertedmouse;
+
+    void Start ()
+    {
+        ////SE ASIGNA ROTACION A UN OBJETO
+        //transform.eulerAngles = new Vector3(30,0,0);       
+	}
+
+	void Update () {
+        int fingerCount = 0;
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)
+                fingerCount++;
+
+        }
+        if (fingerCount > 0)
+        {
+            print("User has " + fingerCount + " finger(s) touching the screen");
+        }
+        hola.text = fingerCount.ToString();
+
+        // //ESTE IF CONTROL O INVIERTE LA ROTACION VERTICAL CON EL MUSE
+        // if (invertedmouse)
+        // {
+        //     mousey = mousey + Input.GetAxis("Mouse Y");
+        // }
+        // else
+        // {
+        //     mousey = mousey - Input.GetAxis("Mouse Y");
+        // }
+
+        ////ROTA EL OBJETO DEPENDIENDO DEL MOVIMIENTO DEL MOUSE VERTICAL 
+        // mousex = mousex + Input.GetAxis("Mouse X"); 
+        // transform.eulerAngles = new Vector3(mousey, mousex, 0);     
+    }   
+}
---
+++ AlienSniper/Assets/AlienSniper/Scrip/LookATZoom.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class LookATZoom : MonoBehaviour {
+
+    bool active;
+    public Image sight;
+	// Use this for initialization
+	void Start () {
+        active = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1"))
+        {
+            print("HOLA");
+            if(active)
+            {
+                Camera.main.fieldOfView = 4f;
+                //gameObject.GetComponent<Camera>().fieldOfView = 4f;
+                active = false;
+            }
+            else
+            {
+                Camera.main.fieldOfView = 60f;
+                active = true;
+            }
+            Sight();
+        }
+	}
+
+    public void Sight()
+    {
+        sight.enabled = !sight.enabled;
+    }
+}
