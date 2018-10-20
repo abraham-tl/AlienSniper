6bd082fda82105bc7cfbaa09f41c9ca2c826ca33
DesmondCF <Canserbero201585@gmail.com>
2018-10-19 19:57:13 -0500

    resubida

---
+++ AlienSniper/Assets/AlienSniper/Scrip/Enemy/NPCS.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCS : MonoBehaviour {
+
+    public float speed;
+    public float timeMax;
+    public float timeRun;
+    int i;
+    private void Start()
+    {
+        timeMax = Random.Range(1,5);
+        speed = Random.Range(1,5);
+    }
+    void Update () {
+        timeRun += Time.deltaTime;
+        if (timeRun >= timeMax)
+        {
+            i = Random.Range(0, 5);
+            timeRun = 0;
+        }
+
+        switch (i)
+        {
+            case 0:
+                transform.position += transform.forward * speed * Time.deltaTime;
+                break;
+            case 1:
+                transform.position -= transform.forward * speed * Time.deltaTime;
+                break;
+            case 2:
+                transform.position += transform.right * speed * Time.deltaTime;
+                break;
+            case 3:
+                transform.position -= transform.right * speed * Time.deltaTime;
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ AlienSniper/Assets/AlienSniper/Scrip/Player/ZoomCamera.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ZoomCamera : MonoBehaviour
+{
+    public bool active_Zoom;
+    public Image lente;
+    public Image sight;
+    private Camera cam;
+    public float max_Zoom = 60f;
+    public float min_Zoom = 5f;
+
+    private void Start()
+    {
+        cam = Camera.FindObjectOfType<Camera>();
+    }
+    void Update () {
+        if (active_Zoom)
+        {
+            cam.fieldOfView -= 500 * Time.deltaTime;
+            if (cam.fieldOfView <= min_Zoom && active_Zoom)
+            {
+                cam.fieldOfView = min_Zoom;
+            }
+        }
+        else
+        {
+            cam.fieldOfView += 500 * Time.deltaTime;
+            if (cam.fieldOfView >= max_Zoom && !active_Zoom)
+            {
+                cam.fieldOfView = max_Zoom;
+            }
+        }
+    }
+    public void Play_Zoom()
+    {
+        if (active_Zoom)
+            active_Zoom = false;
+        else
+            active_Zoom = true;
+        lente.enabled = !lente.enabled;
+        sight.enabled = !sight.enabled;
+    }
+}
